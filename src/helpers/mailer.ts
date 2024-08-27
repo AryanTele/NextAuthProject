803a39eca7a6a41e476484801cf10cd2e7418b80
Aryan Tele <telearyan12@gmail.com>
2024-08-27 18:42:20 +0530

    configuring nodemailer in mailer.ts

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 465,
+      secure: true, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
+
+    // send mail function
+    const mailOptions = {
+      from: "telearyan12@gmail.com", // sender address
+      to: email, // list of receivers
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset your Password", // Assuming we have two mail type
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    };
+
+    const mailResponse = await transporter.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
